96ec13113c86051bcaa9684169c160fe66339d97
lukeforte40 <lukeforte40@gmail.com>
2025-04-09 15:38:43 -0400

    added on click to nav bar hamburger menu need to add animation still

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,6 +3,18 @@ import styles from "../styles/components/nav.module.scss"
 import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function Nav() {
+    // Rotate hamburger and open nav menu
+    const handleHamburgerClick = () => {
+        if (window.getComputedStyle(document.getElementById('navLinks', null)).display === "none") {
+            document.getElementById("navLinks").style.display = "flex";
+            document.getElementById("hamburger").style.transform = "rotate(90deg)"
+        }
+        else{
+            document.getElementById("navLinks").style.display = "none";
+            document.getElementById("hamburger").style.transform = "rotate(0deg)"
+        }
+    }
+
     return(
         <nav>
             <div id={styles.upperNav}>
@@ -13,7 +25,7 @@ export default function Nav() {
                 </div>
             </div>
             <div id={styles.lowerNav}>
-                <RxHamburgerMenu className={styles.hamburger} id="hamburger"/>
+                <RxHamburgerMenu className={styles.hamburger} id="hamburger" onClick={handleHamburgerClick}/>
                 <div className={styles.navLinks} id="navLinks">
                     <NavLink to="/travelJournal">Travel Journal</NavLink>
                 </div>
